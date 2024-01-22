d4be1ad21f6b1a45d90c158e5317bf647bfe142c
mwebe emmanuel kimera <memmar1@gmail.com>
2024-01-22 18:17:04 +0300

    CHANGES

--- my_test/average_calculator.py
+++ my_test/average_calculator.py
@@ -1,11 +1,22 @@
 total = 0
 count = 0
 while True:
-    inp = input("enter an input (type done to finish): ")
+    inp = input("1. enter an input (type done to finish): ")
     if inp.lower() == "done":
         break
     value = float(inp)
     total = total+value
     count = count+1
 average  = total/count
-print ("average of all the inputs: ", average)
+print ("1. average of all the inputs: ", average)
+
+#another method
+numlist = []
+while True:
+    num1 = input("2. enter an input (type done to finish): ")
+    if num1.lower() == "done":
+        break
+    value=float(num1)
+    numlist.append(value)
+average2 = sum(numlist)/len(numlist)
+print ("2. average of all the inputs: ", average2)
--- my_test/list.py
+++ my_test/list.py
@@ -34,3 +34,46 @@ for i in range(len(z)):
 z[2] = 45   #lists are mutable you can change the items in it.
 print(z)
 
+abc = "there are thre words"
+staff = abc.split()
+print(staff)
+
+for r in staff:
+    print(r)
+
+#splitinf
+s = 'spam-spam-spam'
+t = s.split("-")
+print (s)
+print (t)
+
+#another example
+v = "From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
+n = v.split()
+print (n)
+email = n[1]
+print (email)
+lastpart = email.split("@")
+print(lastpart)
+print(lastpart[1])
+
+aa = "banana"
+ab = "banana"
+print("aa == ab:",aa == ab)
+print(aa is ab)
+
+ba = [1,2,3]
+bb = [1,2,3]
+print("ba==bb: ",ba==bb)
+print(ba is bb)
+
+#funct
+def delete_head(t):
+    del t[0]
+
+letters = ['a', 'b', 'c']
+print(letters)
+delete_head(letters)
+print(letters)
+
+
