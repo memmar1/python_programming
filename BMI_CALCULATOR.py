ddf58280b87acd2c4158bda12ee70c83b13dfa00
mwebe emmanuel kimera <memmar1@gmail.com>
2024-02-01 11:00:00 +0300

    made a Body Mass Index calculator

---
+++ BMI_CALCULATOR.py
@@ -0,0 +1,26 @@
+
+name1 = input("Enter you name:" )
+age = int(input("Enter your age"))
+weight = int(input("enter you weight in kgs: "))
+height = int(input("enter you height in m: "))
+
+
+def bni_calculator(name1, weight,height):
+    bni = weight / (height**2)
+    print(f"bmi: ",bni)
+ 
+    if bni < 18:
+        print( name1, "of", age, "year")
+        return "You are under weigth."
+    elif bni < 25:
+        print( name1, "of", age, "year")
+        return "your bni is normal"
+    elif bni < 30:
+        print( name1, "of", age, "year")
+        return "You are over weight."
+    else:
+        print( name1, "of", age, "year")
+        return "You are orbess."
+
+result = bni_calculator(name1, weight,height)
+print(result)
